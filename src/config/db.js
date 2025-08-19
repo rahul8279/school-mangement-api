c667cdf3a1027c8a4f3eaf1e63f3910c8c24be29
rahul8279 <rana827965@gmail.com>
2025-08-19 09:44:05 +0530

    chages for server

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
+  port: process.env.MYSQLPORT,
   database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
