49cbf06367178007b1ed3053a1507c938b20e153
rahul8279 <rana827965@gmail.com>
2025-08-19 10:07:28 +0530

    #

--- src/server.js
+++ src/server.js
@@ -15,9 +15,14 @@ app.use(cors({
 }))
 const PORT = process.env.PORT || 3000 
 
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Server is up and running!" });
+});
+
 app.use("/api/schools", schoolRoutes);
 
-app.listen(PORT, () => {
+// FIX 2: Add '0.0.0.0' to the listen method
+app.listen(PORT, '0.0.0.0', () => {
     connectDB()
     console.log(`Server is running on port ${PORT}`);
 })
