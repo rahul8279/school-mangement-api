b068f6db8799145d2bcb3274bd3059f54906171a
rahul8279 <rana827965@gmail.com>
2025-08-19 10:49:33 +0530

    \

--- src/config/db.js
+++ src/config/db.js
@@ -13,6 +13,14 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 });
+await pool.query(`
+      CREATE TABLE IF NOT EXISTS school (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        address VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `);
 
 export async function connectDB() {
   try {
